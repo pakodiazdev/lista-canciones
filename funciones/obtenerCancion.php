d51d2d3e600d06c70c97456380d891c128fd3973
Pako Diaz <me@pakodiaz.dev>
2026-05-05 13:28:05 -0600

    agrege arreglo de caciones

--- funciones/obtenerCancion.php
+++ funciones/obtenerCancion.php
@@ -1,7 +1,36 @@
 <?php
 
+function obtenerCanciones() {
+    $canciones = [
+        [
+            "ID" => 1,
+            "Titulo" => "Shape of You",
+            "Artista" => "Ed Sheeran",
+            "Album" => "÷ (Divide)",
+            "Favorita" => true
+        ],
+        [
+            "ID" => 2,
+            "Titulo" => "Blinding Lights",
+            "Artista" => "The Weeknd",
+            "Album" => "After Hours",
+            "Favorita" => false
+        ],
+        [
+            "ID" => 3,
+            "Titulo" => "Levitating",
+            "Artista" => "Dua Lipa",
+            "Album" => "Future Nostalgia",
+            "Favorita" => true
+        ],
+        [
+            "ID" => 4,
+            "Titulo" => "Bad Guy",
+            "Artista" => "Billie Eilish",
+            "Album" => "When We All Fall Asleep, Where Do We Go?",
+            "Favorita" => false
+        ]
+    ];
 
-
-function obtenerCancion() {
-    echo "Esta funcion obtendrá las canciones";
+    return $canciones;
 }
